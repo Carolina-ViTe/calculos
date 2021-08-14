d5168c527a94afd120bd444c057c069423ba1e65
carolina tequida <carolina.vicente0.0@gmail.com>
2021-08-14 12:49:02 -0500

    realice unos comentario on el fin de probar POO en una nueva rama

--- codigo.js
+++ codigo.js
@@ -2,8 +2,11 @@
 var select = document.getElementById("figura");
 var operacion = document.getElementById("operacion").value;
 var um = document.getElementById("um").value;
+
 const creacion = document.getElementById("creacion");
+
 const boton_calcular = document.getElementById("calcular");
+
 const cuadrado = document.getElementById("cuadrado");
 const rectangulo = document.getElementById("rectangulo");
 const triangulo = document.getElementById("triangulo");
@@ -40,13 +43,56 @@ select.addEventListener("change", (event) => {
     }else{
         crearInputs();
     }
+
 });
 
+//Con todo esto intento hacerlo por POO pero lo haré en otra rama
+// let figuras = [{
+//     figura: "cuadrado",
+//     area: 1,
+//     areaNombre: "Altura",
+//     perimetro: 1,
+//     perimetroNombre:  "Altura"
+// },{
+//     figura: "rectangulo",
+//     area: 2,
+//     areaNombre1: "Base",
+//     areaNombre2: "Altura",
+//     perimetro: 2,
+//     perimetroNombre1: "Base",
+//     PerimetroNombre2: "Altura"
+// },{
+//     figura: "triangulo",
+//     area: 2,
+//     areaNombre1: "Base",
+//     areaNombre2: "Altura",
+//     perimetro: 3,
+//     perimetroNombre1: "Lado 1",
+//     perimetroNombre2: "Lado 2",
+//     perimetroNombre3: "Lado 3",
+// },{
+//     figura: "circulo",
+//     area: 1,
+//     areaNombre: "Radio",
+//     perimetro: 1,
+//     perimetroNombre: "Radio"
+// }];
+
+// let index = null;
+// for (let i = 0; i < figuras.length; i++){
+//     if(figuras[i].figura === event.target.value){
+//         index = i;
+//         break;
+//     }
+// }
+
 function crearInputs(){
+    
     if(event.target.value == "cuadrado"){
         // var div = document.getElementById("cuadrado");
         var input = document.createElement("INPUT");
         input.type = "number";
+        input.placeholder = "Altura"
         input.classList.add("cuadro");
         cuadrado.appendChild(input);
     } else if(event.target.value == "rectangulo"){
@@ -54,6 +100,8 @@ function crearInputs(){
         var input2 = document.createElement("INPUT");
         input.type = "number";
         input2.type = "number";
+        input.placeholder = "Base";
+        input2.placeholder = "Altura";
         input.classList.add("rect");
         input2.classList.add("rect2");
         rectangulo.appendChild(input);
@@ -65,6 +113,9 @@ function crearInputs(){
         input.type = "number";
         input2.type = "number";
         input3.type = "number";
+        input.placeholder = "Lado 1";
+        input2.placeholder = "Lado 2";
+        input3.placeholder = "Lado 3";
         input.classList.add("tria");
         input2.classList.add("tria2");
         input3.classList.add("tria3");
@@ -74,11 +125,11 @@ function crearInputs(){
     }else if(event.target.value == "circulo"){
         var input = document.createElement("INPUT");
         input.type = "number";
+        input.placeholder = "Radio";
         input.classList.add("cir");
         circulo.appendChild(input);
     }
 }
-
 
 // evento que me permite mostrar el resultado de los calculos obtenidos de 
 boton_calcular.addEventListener("click",(e)=>{
