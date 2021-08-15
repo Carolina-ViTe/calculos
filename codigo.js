149915f5d95b7a49e37bfe973e07311a98f4bb00
carolina tequida <carolina.vicente0.0@gmail.com>
2021-08-15 00:00:45 -0500

    hice unos cambios en la creación de los inputspor medio de la seleccion de opciones

--- codigo.js
+++ codigo.js
@@ -1,6 +1,6 @@
 
 var select = document.getElementById("figura");
-var operacion = document.getElementById("operacion").value;
+var operacion = document.getElementById("operacion");
 var um = document.getElementById("um").value;
 
 const creacion = document.getElementById("creacion");
@@ -18,118 +18,85 @@ const circulo = document.getElementById("circulo");
 
 select.addEventListener("change", (event) => {
     event.preventDefault();
-    if(cuadrado.hasChildNodes()){
-        var cuadro = document.querySelector(".cuadro");
-        cuadrado.removeChild(cuadro);
-        crearInputs();
-    }else if (rectangulo.hasChildNodes()){
-        var rect = document.querySelector(".rect");
-        var rect2 = document.querySelector(".rect2");
-        rectangulo.removeChild(rect);
-        rectangulo.removeChild(rect2);
-        crearInputs();
-    }else if (triangulo.hasChildNodes()){
-        var tria = document.querySelector(".tria");
-        var tria2 = document.querySelector(".tria2");
-        var tria3 = document.querySelector(".tria3");
-        triangulo.removeChild(tria);
-        triangulo.removeChild(tria2);
-        triangulo.removeChild(tria3);
-        crearInputs();
-    }else if (circulo.hasChildNodes()){
-        var cir = document.querySelector(".cir")
-        circulo.removeChild(cir);
-        crearInputs();
-    }else{
-        crearInputs();
+    let index = null;
+    let target = event.target.value;
+    for (let i = 0; i < figuras.length; i++){
+        if(figuras[i].figura === target){
+            index = i;
+            break;
+        }
     }
+    operacion.addEventListener("change", (ev)=>{
+        ev.preventDefault();
+        if(cuadrado.hasChildNodes()){
+            var cuadro = document.querySelector(".cuadrado0");
+            cuadrado.removeChild(cuadro);
+        }else if (rectangulo.hasChildNodes()){
+            var rect = document.querySelector(".rectangulo0");
+            var rect2 = document.querySelector(".rectangulo1");
+            rectangulo.removeChild(rect);
+            rectangulo.removeChild(rect2);
+        }else if (triangulo.hasChildNodes()){
+            var tria = document.querySelector(".triangulo0");
+            var tria2 = document.querySelector(".triangulo1");
+            triangulo.removeChild(tria);
+            triangulo.removeChild(tria2);
+            // if(triangulo.lastChild === document.querySelector(".triangulo2")){
+            //     console.log(triangulo.lastChild);
+            //     var tria3 = document.querySelector(".triangulo2");
+            //     triangulo.removeChild(tria3);
+            // }
+        }else if (circulo.hasChildNodes()){
+            var cir = document.querySelector(".circulo0");
+            circulo.removeChild(cir);
+        }
+        if(operacion.value === "area"){
+            for (let j=0; j<figuras[index].area; j++){
+                var input_j = document.createElement("INPUT");
+                input_j.type = "number";
+                input_j.placeholder = figuras[index].areaNombre[j];
+                input_j.classList.add(target+j);
+                document.getElementById(target).appendChild(input_j);
+            }
+        }else if(operacion.value === "perimetro"){
+            for (let j=0; j<figuras[index].perimetro; j++){
+                var input_j = document.createElement("INPUT");
+                input_j.type = "number";
+                input_j.placeholder = figuras[index].perimetroNombre[j];
+                input_j.classList.add(target+j);
+                document.getElementById(target).appendChild(input_j);
+            }
+        }
+    })
 
 });
 
 //Con todo esto intento hacerlo por POO pero lo haré en otra rama
-// let figuras = [{
-//     figura: "cuadrado",
-//     area: 1,
-//     areaNombre: "Altura",
-//     perimetro: 1,
-//     perimetroNombre:  "Altura"
-// },{
-//     figura: "rectangulo",
-//     area: 2,
-//     areaNombre1: "Base",
-//     areaNombre2: "Altura",
-//     perimetro: 2,
-//     perimetroNombre1: "Base",
-//     PerimetroNombre2: "Altura"
-// },{
-//     figura: "triangulo",
-//     area: 2,
-//     areaNombre1: "Base",
-//     areaNombre2: "Altura",
-//     perimetro: 3,
-//     perimetroNombre1: "Lado 1",
-//     perimetroNombre2: "Lado 2",
-//     perimetroNombre3: "Lado 3",
-// },{
-//     figura: "circulo",
-//     area: 1,
-//     areaNombre: "Radio",
-//     perimetro: 1,
-//     perimetroNombre: "Radio"
-// }];
-
-// let index = null;
-// for (let i = 0; i < figuras.length; i++){
-//     if(figuras[i].figura === event.target.value){
-//         index = i;
-//         break;
-//     }
-// }
-
-function crearInputs(){
-    
-    if(event.target.value == "cuadrado"){
-        // var div = document.getElementById("cuadrado");
-        var input = document.createElement("INPUT");
-        input.type = "number";
-        input.placeholder = "Altura"
-        input.classList.add("cuadro");
-        cuadrado.appendChild(input);
-    } else if(event.target.value == "rectangulo"){
-        var input = document.createElement("INPUT");
-        var input2 = document.createElement("INPUT");
-        input.type = "number";
-        input2.type = "number";
-        input.placeholder = "Base";
-        input2.placeholder = "Altura";
-        input.classList.add("rect");
-        input2.classList.add("rect2");
-        rectangulo.appendChild(input);
-        rectangulo.appendChild(input2);
-    }else if(event.target.value == "triangulo"){
-        var input = document.createElement("INPUT");
-        var input2 = document.createElement("INPUT");
-        var input3 = document.createElement("INPUT");
-        input.type = "number";
-        input2.type = "number";
-        input3.type = "number";
-        input.placeholder = "Lado 1";
-        input2.placeholder = "Lado 2";
-        input3.placeholder = "Lado 3";
-        input.classList.add("tria");
-        input2.classList.add("tria2");
-        input3.classList.add("tria3");
-        triangulo.appendChild(input);
-        triangulo.appendChild(input2);
-        triangulo.appendChild(input3);
-    }else if(event.target.value == "circulo"){
-        var input = document.createElement("INPUT");
-        input.type = "number";
-        input.placeholder = "Radio";
-        input.classList.add("cir");
-        circulo.appendChild(input);
-    }
-}
+let figuras = [{
+    figura: "cuadrado",
+    area: 1,
+    areaNombre: ["Altura"],
+    perimetro: 1,
+    perimetroNombre: ["lado"]
+},{
+    figura: "rectangulo",
+    area: 2,
+    areaNombre: ["Base", "Altura"],
+    perimetro: 2,
+    perimetroNombre: ["Base", "Altura"]
+},{
+    figura: "triangulo",
+    area: 2,
+    areaNombre: ["Base", "Altura"],
+    perimetro: 3,
+    perimetroNombre: ["Lado 1", "Lado 2", "Lado 3"]
+},{
+    figura: "circulo",
+    area: 1,
+    areaNombre: ["Radio"],
+    perimetro: 1,
+    perimetroNombre: ["Radio"]
+}];
 
 // evento que me permite mostrar el resultado de los calculos obtenidos de 
 boton_calcular.addEventListener("click",(e)=>{
