207f7a199b2ff6e683bd550b31f24ac23eec770c
carolina tequida <carolina.vicente0.0@gmail.com>
2021-08-16 18:05:52 -0500

    Esta casi listo solo falta hacer bien las formulas para los calculos

--- codigo.js
+++ codigo.js
@@ -1,10 +1,10 @@
-// var select = document.getElementById("figura");                     //Este me da la seleccion de figura del usuario
-// var operacion = document.getElementById("operacion");               //Este me da la selección de operación del usuario
-
+// Esto sson los div donde apareceran los inputs dependiendo de la figura seleccionada
 const div_cuadrado = document.getElementById("cuadrado");               //Este es el div para crear inputs para cuadrado
 const div_rectangulo = document.getElementById("rectangulo");           //Este es el div para crear inputs para rectangulo
 const div_triangulo = document.getElementById("triangulo");             //Este es el div para crear inputs para triangulo
 const div_circulo = document.getElementById("circulo");                 //Este es el div para crear inputs para circulo
+
+const boton_calcular = document.getElementById("calcular");
 
 const FIGURAS = [{
     figura: "cuadrado",
@@ -35,8 +35,10 @@ const FIGURAS = [{
 
 
 function fig_select(){
-    var select = document.getElementById("figura").value; 
-    var operacion = document.getElementById("operacion").value;  
+    var select = document.getElementById("figura").value;               // Me da la opción de la etiqueta select con el div figura que se haya seleccionada 
+    var operacion = document.getElementById("operacion").value;         // Me da la opción de la etiqueta select con el div operacion que se haya seleccionada
+    var unidad_de_medida = document.getElementById("um").value;
+
     let index = null;
     for (let i = 0; i < FIGURAS.length; i++){
         if(FIGURAS[i].figura === select){
@@ -45,52 +47,75 @@ function fig_select(){
         }
     }
     console.log(select, operacion);
+    let htmlCode="";
+    let htmlCode2="";
 
     if(operacion === "area"){
+        // Aqui se invoca la función para borrar inputs que hayan sido creados por otra opción
         delete_inputs();
+        // Aqui se crean los inputs en caso de que se quiera calcular el area
         for (let j=0; j<FIGURAS[index].area; j++){
-            var input_j = document.createElement("INPUT");
-            input_j.type = "number";
-            input_j.placeholder = FIGURAS[index].areaNombre[j];
-            input_j.classList.add(select+j);
-            document.getElementById(select).appendChild(input_j);
+            htmlCode += `<input type="number" class=${select+j} placeholder=${FIGURAS[index].areaNombre[j]}>`; 
         }
+        document.getElementById(select).innerHTML = htmlCode;
     }else if(operacion === "perimetro"){
+        // Aqui se invoca la función para borrar inputs que hayan sido creados por otra opción
         delete_inputs();
+        // Aqui se crean los inputs en caso de que se quiera calcular el perimetro
         for (let j=0; j<FIGURAS[index].perimetro; j++){
-            var input_j = document.createElement("INPUT");
-            input_j.type = "number";
-            input_j.placeholder = FIGURAS[index].perimetroNombre[j];
-            input_j.classList.add(select+j);
-            document.getElementById(select).appendChild(input_j);
+            htmlCode += `<input type="number" class=${select+j} placeholder=${FIGURAS[index].perimetroNombre[j]}>`; 
         }
+        document.getElementById(select).innerHTML = htmlCode;
     }
+
+    boton_calcular.addEventListener("click",()=>{
+        let area = 1;
+        let perimetro = 0;
+
+        if(operacion === "area"){
+            for (let c=0; c<FIGURAS.length; c++){
+                if(document.getElementById(FIGURAS[c].figura).hasChildNodes()){
+                    children = document.getElementById(FIGURAS[c].figura).childNodes;
+                    for (let d=0; d<children.length; d++){
+                        let identificador = "." + FIGURAS[c].figura + d
+                        let valor_d = document.querySelector(identificador);
+                        area = area * parseFloat(valor_d.value);
+                    }
+                }
+            } 
+            htmlCode2 = `
+            <h3 id="resultado_total">Área = ${area} ${unidad_de_medida}2</h3>`   
+        } else if (operacion === "perimetro"){
+            for (let c=0; c<FIGURAS.length; c++){
+                if(document.getElementById(FIGURAS[c].figura).hasChildNodes()){
+                    children = document.getElementById(FIGURAS[c].figura).childNodes;
+                    for (let d=0; d<children.length; d++){
+                        let identificador = "." + FIGURAS[c].figura + d
+                        let valor_d = document.querySelector(identificador);
+                        perimetro += parseFloat(valor_d.value);
+                    }
+                }
+            } 
+            htmlCode2 = `
+            <h3 id="resultado_total">Perímetro= ${perimetro} ${unidad_de_medida}</h3>` 
+        }
+        document.getElementById("resulta").innerHTML = htmlCode2;
+    })
 }
 
+// Función para borrar inputs en caso de que se requiera
 function delete_inputs(){
-    if(div_cuadrado.hasChildNodes()){
-        while(div_cuadrado.firstChild){
-            div_cuadrado.removeChild(div_cuadrado.firstChild)
-        }
-    }else if (div_rectangulo.hasChildNodes()){
-        while(div_rectangulo.firstChild){
-            div_rectangulo.removeChild(div_rectangulo.firstChild)
-        }
-    }else if (div_triangulo.hasChildNodes()){
-        while(div_triangulo.firstChild){
-            div_triangulo.removeChild(div_triangulo.firstChild)
-        }
-    }else if (div_circulo.hasChildNodes()){
-        while(div_circulo.firstChild){
-            div_circulo.removeChild(div_circulo.firstChild)
+    for (let a=0; a<FIGURAS.length; a++){
+        if(document.getElementById(FIGURAS[a].figura).hasChildNodes()){
+            while(document.getElementById(FIGURAS[a].figura).firstChild){
+                document.getElementById(FIGURAS[a].figura).removeChild(document.getElementById(FIGURAS[a].figura).firstChild)
+            }
         }
     }
+
+    
 }
- 
         
         
-// i, j, k
-        
-        
-        
+// i, j, k, a, b, c, d
         
